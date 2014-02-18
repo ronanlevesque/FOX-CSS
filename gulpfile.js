a0e85f7e25e8ad3a8fba5e7b3a2bcb78a36c59d8
Ronan Levesque <ronan.levesque@gmail.com>
2014-02-18 15:05:05 +0100

    Removed `gulp.run` from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 var gulp = require('gulp'),
 	sass = require('gulp-ruby-sass'),
 	autoprefixer = require('gulp-autoprefixer');
- 
+
 gulp.task('sass', function(){
 	return gulp.src(['./sass/**/*.scss', '!./sass/**/_*.scss'])
 	.pipe(sass({style: 'expanded'}))
 	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 	.pipe(gulp.dest('./'));
 });
- 
+
+gulp.task('watch', function() {
+	gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
 gulp.task('watch', function() {
 	gulp.watch('./sass/**/*.scss', function(event) {
 		console.log('File ' + event.path + ' ' + event.type);
@@ -16,6 +20,4 @@ gulp.task('watch', function() {
 	});
 });
  
-gulp.task('default', function () {
-	gulp.run('watch');
-});
+gulp.task('default', ['watch']);
