e6d2b4ede14de1ebae8686a9be6222d5f1420165
Ronan Levesque <ronan.levesque@gmail.com>
2015-04-13 11:41:38 +0200

    Better gulpfile with errors handled

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,24 @@
 var gulp = require('gulp');
 var	sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
 
-gulp.task('sass', function(){
-	return gulp.src(['./sass/**/*.scss', '!./sass/**/_*.scss'])
-	.pipe(sass({style: 'expanded'}))
-	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-	.pipe(gulp.dest('./'));
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
+  .pipe(sass())
+  .on('error', notify.onError("Error: <%= error.message %>"))
+  .on('error', handleError)
+  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+  .pipe(gulp.dest('./'))
 });
 
 gulp.task('watch', function() {
 	gulp.watch('./sass/**/*.scss', ['sass']);
-});
-
-gulp.task('watch', function() {
-	gulp.watch('./sass/**/*.scss', function(event) {
-		console.log('File ' + event.path + ' ' + event.type);
-		gulp.run('sass');
-	});
 });
 
 gulp.task('default', ['watch']);
